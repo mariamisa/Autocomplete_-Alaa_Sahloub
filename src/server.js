2392c971c0e1d14724fbc052059310ef9a7d12a4
Alaa <alaa.sahloub.eb@gmail.com>
2020-11-29 10:08:23 +0200

    seperate server and router

--- src/server.js
+++ src/server.js
@@ -1,66 +1,7 @@
 const http = require('http');
-
+const router = require('./router');
 const port = 3000;
-const path = require('path');
-const fs = require('fs');
-const querystring = require('querystring');
 
-const router = (request, response) => {
-  const endpoint = request.url;
-  if (endpoint === '/') {
-    const filepath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(filepath, (error, file) => {
-      if (error) {
-        response.writeHead(500, { 'content-Type': 'text/html' });
-        response.end('<h1>internal server error</h1>');
-      } else {
-        response.writeHead(200, { 'content-Type': 'text/html' });
-        response.end(file);
-      }
-    });
-  } else if (endpoint.includes('public')) {
-    const extention = endpoint.split('.')[1];
-    const extensionType = {
-      css: 'text/css',
-      js: 'application/javascript',
-      ico: 'image/x-ico',
-      png: 'image/png',
-      html: 'text/html',
-      jpeg: 'image/jpeg',
-      jpg: 'image/jpg',
-    };
-    const filepath = path.join(__dirname, '..', endpoint);
-    fs.readFile(filepath, (error, file) => {
-      if (error) {
-        response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.end('<h1>Internal server error.</h1>');
-      } else {
-        response.writeHead(200, { 'content-Type': extensionType[extention] });
-        response.end(file);
-      }
-    });
-  } else if( endpoint.includes('/search')){
-    const queryEndPoint = endpoint.split('?')[1];
-    const {query} = querystring.parse(queryEndPoint);
-    const filepath = path.join(__dirname, '.', 'words.txt');
-    fs.readFile(filepath, 'utf8' , (error ,contents)=>{
-      if(error){
-        response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.end('<h1>Internal server error.</h1>');
-      }else{
-       const wordsArr = contents.split("\r\n");
-        const result = wordsArr.filter((word)=> word.startsWith(query)).slice(0,20)
-        console.log(query,result);
-        // response.writeHead(200, {'Content-Type' : 'application/json'})
-       response.end(JSON.stringify({result}));
-      }
-    })
-  }else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.write('note found');
-    response.end();
-  }
-};
 
 const server = http.createServer(router);
 server.listen(port, () => {
